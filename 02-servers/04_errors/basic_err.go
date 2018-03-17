495d30aeb04da178bee4e465ce32a8898e3f09ca
Max Prosper <maxim.yaaman@gmail.com>
2018-03-17 20:41:53 +0200

    Error handling

---
+++ 02-servers/04_errors/basic_err.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+var (
+	client = http.Client{Timeout: time.Duration(time.Millisecond)}
+)
+
+func getRemoteResource() error {
+	url := "http://127.0.0.1:9999/pages?id=123"
+	_, err := client.Get(url)
+	if err != nil {
+
+		return fmt.Errorf("getRemoteResource: %s at %s", err, url)
+	}
+	return nil
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	err := getRemoteResource()
+
+	if err != nil {
+		fmt.Printf("error happend: %+v\n", err)
+		http.Error(w, "internal error", 500)
+		return
+	}
+
+	w.Write([]byte("all is OK"))
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	fmt.Println("starting server at :8080")
+	http.ListenAndServe(":8080", nil)
+}
